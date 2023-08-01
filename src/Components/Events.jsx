de6ce91e277ae6ebbe2d79e277ec774fba69337f
Said-88 <139504036+Said-88@users.noreply.github.com>
2023-07-31 23:04:25 -0600

    feat: Home (#2)

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,11 +1,23 @@
 
-
-export const Events = () => {
-
+const EventCard = ({ title }) => {
+  const handleAccessEvent = () => {
+    // Aquí agregar la lógica para redirigir a la página del evento.
+  };
 
   return (
-    <>
-        <div>Events</div>
-    </>
-  )
-}
+    <div className="bg-white rounded-lg overflow-hidden p-4 shadow-lg transition-transform duration-300 transform hover:scale-105 hover:shadow-lg">
+      <img className="w-full h-40 object-cover mb-4" src={"https://www.usap.edu/wp-content/uploads/2023/07/Banner-Web-Matricula-Gratis-45-aniversario-USAP.png"} alt={title} style={{ width: '100%', height: '100%' }} />
+      <h3 className="text-xl font-semibold mb-2">{"Proyecto de Vinculación: Voluntariado ASJ"}</h3>
+      <p className="text-gray-600">{"Fri, Aug 4, 8:30 AM + 104 more events"}</p>
+      <p className="text-gray-1300 mt-1">Usap • San Pedro Sula</p>
+      <button
+        onClick={handleAccessEvent}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+      >
+        Acceder al evento
+      </button>
+    </div>
+  );
+};
+
+export default EventCard;
